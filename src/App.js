b1dbafd09bfb367b5dc25af2dac8d1d7a08da966
didin21 <tanashikovadina@gmail.com>
2022-02-19 00:16:47 -0800

    post publish

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import TopBar from "./topbar/TopBar";
+import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
-
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 function App() {
   return (
     <>
    <TopBar/>
-    <Home/>
+    <Write/>
 
    </>
   );
